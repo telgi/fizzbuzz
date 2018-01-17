1a7591e620f61b63044abcf1bc900369a77b4e7f
Hannah Lillis <hannahamanda16@gmail.com>
2018-01-17 16:51:50 +0000

    Refactoring if/elsif/else to a case/when statement

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,11 @@
 def fizzbuzz(number)
-  if number == 0
-    number
-  elsif number % 3 == 0 && number % 5 == 0
-    'fizzbuzz'
-  elsif number % 3 == 0
-    'fizz'
-  elsif number % 5 == 0
-    'buzz'
-  else
-    number
-  end
+
+case
+  when number == 0 then number
+  when number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
+  when number % 3 == 0 then 'fizz'
+  when number % 5 == 0 then 'buzz'
+  else number
+end
+
 end
