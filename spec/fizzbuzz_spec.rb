d21704cd04dff528281f3f65a5de55d8d06d9263
Terry Allitt <tallitt88@gmail.com>
2018-01-17 16:35:27 +0000

    add test to return 'fizzbuzz' when passing a multiple of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,4 +20,8 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed a multiple of 5' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end
+
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
