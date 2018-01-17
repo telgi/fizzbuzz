dff6fb6e5b8cae7c4324ba5791eca3c0eb177e3a
Hannah Lillis <hannahamanda16@gmail.com>
2018-01-17 16:30:12 +0000

    Create test 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,4 +16,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(9)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
 end
